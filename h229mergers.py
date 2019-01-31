a42326147b2a947d3d16e722e63ac9453a036d5b
SarraHayoune <35125070+SarraHayoune@users.noreply.github.com>
2019-01-31 16:23:30 -0500

    try

---
+++ h229mergers.py
@@ -0,0 +1,28 @@
+import pynbody 
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import readcol
+
+
+files = readcol.readcol('/Jillian/h229/files.list')
+files = files[:,0]
+def getz(s):
+    return s.properties['z']
+    
+f =  open("h229.dat", "w+") 
+for file in files:
+    
+    # loading the snapshotS
+    s =pynbody.load('/Jillian/h229/'+file)
+    print s
+ data = [getz(s)] 
+        
+        
+        data= str(data)
+        data= data[1:-1]
+        f.write(data+'\n')
+    
+    print data
+         
+f.close()

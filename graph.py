2c3fa2770786007fb0c1722e89edaa147051b30f
SARRA HAYOUNE <35125070+SarraHayoune@users.noreply.github.com>
2019-03-21 11:37:56 -0400

    new file

---
+++ graph.py
@@ -0,0 +1,30 @@
+import pynbody
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import readcol
+
+
+files = readcol.readcol('testdata.txt')
+Time= files[:,0]
+BHmass = files [:,2]
+BHDistance= files[:,3]
+
+#plt.plot(Time, BHDistance)
+#plt.ylabel('BH Distance')
+#plt.xlabel('Time')
+
+
+plt.xlabel("Time 'Gyr'")
+plt.ylabel("BH Mass 'Kpc'")
+plt.plot(Time, BHmass,"b-", linewidth=2)
+plt.tick_params(axis="BHmass", labelcolor="b")
+
+plt.twinx()
+plt.ylabel("BH Distance 'Kpc'")
+plt.plot(Time, BHDistance, "r-", linewidth=2)
+
+plt.tick_params(axis="BHdistance", labelcolor="r")
+
+plt.show()

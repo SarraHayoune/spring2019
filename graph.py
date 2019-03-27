5f584711c826c89e64d81073b8dac1ef70df971e
SARRA HAYOUNE <35125070+SarraHayoune@users.noreply.github.com>
2019-03-27 15:34:41 -0400

    commit manually

--- graph.py
+++ graph.py
@@ -2,29 +2,43 @@ import pynbody
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib
 import readcol
 
 
-files = readcol.readcol('testdata.txt')
-Time= files[:,0]
-BHmass = files [:,2]
-BHDistance= files[:,3]
+files1 = readcol.readcol('60352986.fits.txt' )
+files2 = readcol.readcol('60354630.fits.txt' )
 
-#plt.plot(Time, BHDistance)
-#plt.ylabel('BH Distance')
-#plt.xlabel('Time')
+maxtime=max(files2[:,0])
+print maxtime
+
+index= np.where(files1[:,0] <= maxtime)
+
+x_position1= files1[:,1]
+x_position= x_position1[index]
+
+y_position1= files1[:,1]
+y_position= y_position1[index]
 
 
-plt.xlabel("Time 'Gyr'")
-plt.ylabel("BH Mass 'Kpc'")
-plt.plot(Time, BHmass,"b-", linewidth=2)
-plt.tick_params(axis="BHmass", labelcolor="b")
+X_POSITION2 = files2[:,1]
 
+Y_POSITION2= files2[:,2]
+#Y_POSITION= np.array(Y_POSITION)
+
+plt.plot(x_position, y_position)
+plt.ylabel("Y-POSITION for 60352986 " )
+plt.xlabel("X-POSITION")
+
+plt.tick_params(axis="Y-POSITION for 60354630", labelcolor= "b")
+
+plt.plot(X_POSITION2,Y_POSITION2)
 plt.twinx()
-plt.ylabel("BH Distance 'Kpc'")
-plt.plot(Time, BHDistance, "r-", linewidth=2)
+plt.ylabel("Y-POSITION for 60354630")
 
-plt.tick_params(axis="BHdistance", labelcolor="r")
+#plt.plot(X_POSITION1,Y_POSITION1)
+plt.tick_params(axis="Y-POSITION for 60352986", labelcolor= "r")
+
+
+plt.savefig(filename= 'pplot1'+'.png')
 
 plt.show()

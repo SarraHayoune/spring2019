71c6735d01f1d0ad6d3c28be2402136c4a11eb67
SARRA HAYOUNE <35125070+SarraHayoune@users.noreply.github.com>
2020-07-20 01:06:02 -0400

    add description and comments

--- graph_2bh_pos.py
+++ graph_2bh_pos.py
@@ -1,3 +1,5 @@
+# making a graph of the position of two black holes
+
 import pynbody
 import numpy as np
 import pandas as pd
@@ -7,12 +9,14 @@ import readcol
 
 files1 = readcol.readcol('60352986.fits.txt' )
 files2 = readcol.readcol('60354630.fits.txt' )
+# limit the time for one black hole, we are interested in the period until the merge happen not after
 
 maxtime=max(files2[:,0])
 print maxtime
 
 index= np.where(files1[:,0] <= maxtime)
 
+# set the x & y positions from black holes files
 x_position1= files1[:,1]
 x_position= x_position1[index]
 
@@ -21,10 +25,9 @@ y_position= y_position1[index]
 
 
 X_POSITION2 = files2[:,1]
-
 Y_POSITION2= files2[:,2]
 #Y_POSITION= np.array(Y_POSITION)
-
+# plotting x & y positions of both black holes in one graph
 plt.plot(x_position, y_position)
 plt.ylabel("Y-POSITION for 60352986 " )
 plt.xlabel("X-POSITION")
